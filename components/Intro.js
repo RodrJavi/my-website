8c45494ba39fbe46776af04c17e95586dc6d7561
RodrJavi <nb2javi@gmail.com>
2022-06-22 21:56:12 -0500

    pc transfer 6/22

--- components/Intro.js
+++ components/Intro.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Button from "./Button"
+import {Icon} from "@iconify/react"
 
 const Intro = () => {
-  return <div>
+  return <div className="hero-container">
     <div className="hero-card">
       <span className="hero-name">Javier.</span>
       <span className="hero">Web Developer.</span>
@@ -11,6 +12,12 @@ const Intro = () => {
     <div className="name-buttons">
       <Button text="See my Work"></Button>
       <Button text="Get in touch" outline="outline"></Button>
+    </div>
+
+    <div>
+    <Icon className="icons" icon="mdi:github"></Icon>
+    <Icon className="icons" icon="mdi:linkedin"></Icon>
+    <Icon className="icons" icon="mdi:instagram"></Icon>
     </div>
 
   </div>;
